638342a3972b53f06cdb696c6783009741e698d7
Bravelike <bunybravelike23@gmail.com>
2025-10-05 15:21:14 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('registration-form');
+    const feedbackDiv = document.getElementById('form-feedback')
+
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const name = document.getElementById('username').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password').value.trim();
+    
+
+    let isValid = true;
+    let messages = [];
+
+    if (name.length < 3) {
+        isValid = false;
+        messages.push("Username must be at least 3 characters long.")
+    }
+
+    if (!email.includes("@") || !email.includes(".")) {
+        isValid = false;
+        messages.push("Please input a valid Email")
+    }
+
+    if (password.length < 8) {
+        isValid = false;
+        messages.push("Password must be atleast eight characters long.")
+    }
+
+    if (!isValid) {
+        feedbackDiv.innerHTML = messages.join("<br>");
+        feedbackDiv.className = "error";
+        feedbackDiv.style.display = "block"
+    } else {
+        feedbackDiv.innerHTML = "Registration Successful";
+        feedbackDiv.style.color = "#28a745";
+        form.reset();
+
+    }
+    })
+
+    
+})
